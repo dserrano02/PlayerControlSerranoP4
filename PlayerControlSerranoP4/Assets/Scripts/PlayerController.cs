7be93830a42f922897b68a53a203c38eea02f9f7
dserrano02 <116206499+dserrano02@users.noreply.github.com>
2023-02-16 11:37:03 -0800

    Made new scripts and made car move.
    I made a script for my car and camera.

--- PlayerControlSerranoP4/Assets/Scripts/PlayerController.cs
+++ PlayerControlSerranoP4/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 30.0f;
+    private float turnSpeed = 100.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
-    // Update is called once per frame
-    void Update()
+
+    void Updatee()
     {
-        // move the vehiccle foward
-        transform.Translate(Vector3.forward*Time.deltaTime * 20);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
